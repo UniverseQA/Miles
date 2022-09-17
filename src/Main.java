318ff9a85496c3664eca66816353c282a2377b95
Vladislav Hudasov <vladislavhudasov@yandex.ru>
2022-09-17 19:43:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 6200;
+        int miles = ticket / 20;
+
+        System.out.println("При стоимости билета " + ticket + "$, вы получаете " + miles + " бонусных миль на счёт");
+
+    }
+}
